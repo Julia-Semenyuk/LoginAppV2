ba605ef7bbf868a7780cfa6f741203695f0a0f81
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-08-14 21:39:23 +0900

    Initial Commit

---
+++ LoginAppV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginAppV2
+//
+//  Created by Юлия on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
