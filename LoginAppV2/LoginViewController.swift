649c832a6347ac2f00bc84d099ec96c729ee8352
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-08-15 01:32:06 +0900

    1.1

---
+++ LoginAppV2/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  LoginAppV2
+//
+//  Created by Юлия on 14.08.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    @IBOutlet var forgotUserNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let welcomeVC = segue.destination as? WelcomeViewController {
+            welcomeVC.userName = userNameTF.text
+        }
+    }
+}
+
+
+
---
+++ LoginAppV2/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  LoginAppV2
+//
+//  Created by Юлия on 14.08.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    var userName: String!
+    var password: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
